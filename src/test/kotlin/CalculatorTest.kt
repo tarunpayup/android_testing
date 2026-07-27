d59c10ebcedcae6a336b55d3faa9bbafc3e50498
tarun <tarunpayup@outlook.com>
2026-07-27 20:11:31 +0530

    new push

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -26,5 +26,21 @@ class CalculatorTest {
         assertEquals(expected,actual)
     }
 
+    @ParameterizedTest
+    @CsvSource(
+        "100,10,90",
+        "1000,100,900",
+        "-100,10,-90",
+        "-100,-10,-110"
+    )
+    fun subtractionTest(
+        number1:Int,
+        number2:Int,
+        expected:Int
+    ){
+        val result = calculator.subtraction(number1,number2)
+        assertEquals(expected,result)
+    }
+
 
 }
